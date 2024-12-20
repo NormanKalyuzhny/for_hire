8d34be53c5452e6401e00d72f22d73b360b27d6e
NormanKalyuzhny <innerspiritu@gmail.com>
2024-12-20 21:16:58 +0200

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import brandLogo from '../../assets/svg/logo.svg';
-import burgerBtn from '../../assets/svg/burger-menu.svg'
+import burgerBtn from '../../assets/svg/burger-menu.svg';
 import styles from './Header.module.css';
 
 const menuItems = [
