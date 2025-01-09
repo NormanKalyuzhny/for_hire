87db7c1a4baa2945820485654f5c01e65d4f06a1
NormanKalyuzhny <innerspiritu@gmail.com>
2025-01-09 16:00:31 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,6 @@ export default function App() {
                   />
               </TodoContainer>
             </ComponentBox>
-            
           <ComponentBox title="CSS">
             <ProfileCard/> 
           </ComponentBox>
