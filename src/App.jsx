963f3a71b3db0b9b5b44bd406921bc5707ae1a08
NormanKalyuzhny <innerspiritu@gmail.com>
2025-01-10 23:24:09 +0200

    css grid

--- src/App.jsx
+++ src/App.jsx
@@ -20,113 +20,89 @@ import Card from "./components/Card/Card.jsx";
 import DigitalClock from "./components/DigitalClock/DigitalClock.jsx";
 
 export default function App() {
-  const [todos, setTodos] = useState([])
-  const [todoValue,  setTodoValue] = useState('')
-  const [showSidebar, setShowSidebar] = useState(false)
-  const [isDark, setIsDark] = useLocalStorage("isDark", false)
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDark, setIsDark] = useLocalStorage("isDark", false);
 
-  function persistData(newList){
-    localStorage.setItem('todos', JSON.stringify({todos:
-      newList
-    }))
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }));
   }
 
-  function handleAddTodos(newTodo){
-    const newTodoList = [...todos, newTodo]
-    persistData(newTodoList)
-    setTodos(newTodoList)
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
+    setTodos(newTodoList);
   }
 
-  function handleDeleteTodo(index){
-    const newTodoList = todos.filter((todo, todoIndex)=>{
-      return todoIndex !== index
-    })
-    persistData(newTodoList)
-    setTodos(newTodoList)
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index;
+    });
+    persistData(newTodoList);
+    setTodos(newTodoList);
   }
 
-  function handleEditTodo(index){
-    const valueToBeEdited = todos[index]
-    setTodoValue(valueToBeEdited)
-    handleDeleteTodo(index)
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index];
+    setTodoValue(valueToBeEdited);
+    handleDeleteTodo(index);
   }
 
-  useEffect(()=>{
-    if(!localStorage){
-      return 
+  useEffect(() => {
+    if (!localStorage) {
+      return;
     }
-    let localTodos = localStorage.getItem('todos')
-    if(!localTodos){
-      return
+    let localTodos = localStorage.getItem("todos");
+    if (!localTodos) {
+      return;
     }
-    localTodos = JSON.parse(localTodos).todos
-    setTodos(localTodos)
-  }, [])
-  
-  function handleShowSidebar(){
-    setShowSidebar(!showSidebar)
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
+
+  function handleShowSidebar() {
+    setShowSidebar(!showSidebar);
   }
 
-  const stylesMain = {
-    display: "flex",
-    flexFlow:"wrap",
-    alignSelf: "center",
-    justifyContent:"center",
-    width:"85%",
-    margin: "0 0.5rem",
-    boxSizing: "border-box",
-  }
-
-  return(
-    <div className="wrapper" data-theme={isDark ? "light":"dark"}>
-      <Page1/>
-      <Header handleShowSidebar={handleShowSidebar}/>
-      <DarkMode isChecked={isDark} handleChange={()=>setIsDark(!isDark)}/>
-      <SVGNameAnimation/>
-        <div className="mainContent" style={stylesMain}>
-          {showSidebar && (
-            <Sidebar handleShowSidebar={handleShowSidebar}/>
-          )}
-          <div className="container-box">
-            <ComponentBox title="APIFetch">
-              <APIFetch/>
-            </ComponentBox>
-            <ComponentBox title="Todo">
-              <TodoContainer>
-                  <TodoInput 
-                    todoValue = {todoValue} 
-                    setTodoValue={setTodoValue}
-                    handleAddTodos = {handleAddTodos}
-                  />
-                  <TodoList 
-                    handleDeleteTodo = {handleDeleteTodo} 
-                    handleEditTodo = {handleEditTodo}
-                    todos = {todos}
-                  />
-              </TodoContainer>
-            </ComponentBox>
-          <ComponentBox title="CSS">
-            <ProfileCard/> 
-          </ComponentBox>
-          </div>
-          <ComponentBox title="Simple version">
-            <Card/>
-          </ComponentBox>
-          <ComponentBox title="CSS">
-            <DimensionCube/>
-          </ComponentBox>  
-          <ComponentBox title="CSS">
-            <Circles/>
-          </ComponentBox>
-          <ComponentBox title="Authorization">
-            <AuthPage/>
-          </ComponentBox>
-          <ComponentBox title="Digital Clock">
-            <DigitalClock/>
-          </ComponentBox>
-          <Droid/>
-        </div>
-      <Footer/>
+  return (
+    <div className="wrapper" data-theme={isDark ? "light" : "dark"}>
+      <Page1 />
+      <Header handleShowSidebar={handleShowSidebar} />
+      <DarkMode isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
+      <SVGNameAnimation />
+      <div className="mainContent">
+        {showSidebar && <Sidebar handleShowSidebar={handleShowSidebar} />}
+        <ComponentBox title="APIFetch">
+          <APIFetch />
+        </ComponentBox>
+        <ComponentBox title="Todo">
+          <TodoContainer>
+            <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
+            <TodoList handleDeleteTodo={handleDeleteTodo} handleEditTodo={handleEditTodo} todos={todos} />
+          </TodoContainer>
+        </ComponentBox>
+        <ComponentBox title="CSS">
+          <ProfileCard />
+        </ComponentBox>
+        <ComponentBox title="Simple version">
+          <Card />
+        </ComponentBox>
+        <ComponentBox title="CSS">
+          <DimensionCube />
+        </ComponentBox>
+        <ComponentBox title="CSS">
+          <Circles />
+        </ComponentBox>
+        <ComponentBox title="Authorization">
+          <AuthPage />
+        </ComponentBox>
+        <ComponentBox title="Digital Clock">
+          <DigitalClock />
+        </ComponentBox>
+        <Droid />
+      </div>
+      <Footer />
     </div>
   );
 }
