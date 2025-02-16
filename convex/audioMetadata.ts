b10f3008a7f58d972a0204e2c99aac0196a0ad0c
NormanKalyuzhny <innerspiritu@gmail.com>
2025-02-16 22:40:56 +0200

    check 2

---
+++ convex/audioMetadata.ts
@@ -0,0 +1,9 @@
+// convex/audioMetadata.ts
+import { query } from "./_generated/server";
+
+export const get = query({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query("audioMetadata").collect();
+  },
+});
