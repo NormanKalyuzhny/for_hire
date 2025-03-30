1da2e4580d9e33982c56d69c56d32ec30d8cabae
NormanKalyuzhny <innerspiritu@gmail.com>
2025-03-30 18:02:43 +0300

    fix

--- src/components/DownloadBtn.tsx
+++ src/components/DownloadBtn.tsx
@@ -3,14 +3,14 @@ export default function DownloadBtn() {
   const {t} = useTranslation('btnDownload')
   
   const handleDownload = () => {
-    window.location.href = "https://www.dropbox.com/scl/fi/7iwqezxnbo5lymhd0cmsw/FlipTheCard.apk?rlkey=f039523uhejhwkne8rw48ri5x&st=hvpqggcj&dl=0";
+    window.location.href = "";
   };
 
   return (
     <div>
       <button 
         className="download-btn btn-d-color flex-center-all w-[110px] h-[40px] rounded-[0.5rem] text-[1rem] text-white font-semibold disabled:bg-container disabled:border-b-gray-500 disabled:cursor-not-allowed"
-        onClick={handleDownload} >{t('btnD')}
+        onClick={handleDownload} disabled >{t('btnD')}
       </button>
     </div>
   );
